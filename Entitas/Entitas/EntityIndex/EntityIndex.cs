27692a34151ea219d3d87f350fa62e5d79764178
Bruno <bruno@gabsee.com>
2021-02-04 15:17:22 +0100

    Fix entity index empty hashset creation

--- Entitas/Entitas/EntityIndex/EntityIndex.cs
+++ Entitas/Entitas/EntityIndex/EntityIndex.cs
@@ -32,7 +32,18 @@ namespace Entitas {
             indexEntities(_group);
         }
 
+        private static HashSet<TEntity> _emptyIndex = new HashSet<TEntity>();
+
         public HashSet<TEntity> GetEntities(TKey key) {
+            HashSet<TEntity> entities;
+            if (!_index.TryGetValue(key, out entities)) {
+                return _emptyIndex;
+            }
+
+            return entities;
+        }
+
+        private HashSet<TEntity> GetEntitiesInternal(TKey key) {
             HashSet<TEntity> entities;
             if (!_index.TryGetValue(key, out entities)) {
                 entities = new HashSet<TEntity>(EntityEqualityComparer<TEntity>.comparer);
@@ -40,6 +51,35 @@ namespace Entitas {
             }
 
             return entities;
+        }
+
+        protected override void addEntity(TKey key, TEntity entity) {
+            GetEntitiesInternal(key).Add(entity);
+
+            var safeAerc = entity.aerc as SafeAERC;
+            if (safeAerc != null) {
+                if (!safeAerc.owners.Contains(this)) {
+                    entity.Retain(this);
+                }
+            } else {
+                entity.Retain(this);
+            }
+        }
+
+        protected override void removeEntity(TKey key, TEntity entity) {
+            var hashSet = GetEntitiesInternal(key);
+            hashSet.Remove(entity);
+            if (hashSet.Count == 0)
+                _index.Remove(key);
+
+            var safeAerc = entity.aerc as SafeAERC;
+            if (safeAerc != null) {
+                if (safeAerc.owners.Contains(this)) {
+                    entity.Release(this);
+                }
+            } else {
+                entity.Release(this);
+            }
         }
 
         public override string ToString() {
@@ -61,32 +101,6 @@ namespace Entitas {
             }
 
             _index.Clear();
-        }
-
-        protected override void addEntity(TKey key, TEntity entity) {
-            GetEntities(key).Add(entity);
-
-            var safeAerc = entity.aerc as SafeAERC;
-            if (safeAerc != null) {
-                if (!safeAerc.owners.Contains(this)) {
-                    entity.Retain(this);
-                }
-            } else {
-                entity.Retain(this);
-            }
-        }
-
-        protected override void removeEntity(TKey key, TEntity entity) {
-            GetEntities(key).Remove(entity);
-
-            var safeAerc = entity.aerc as SafeAERC;
-            if (safeAerc != null) {
-                if (safeAerc.owners.Contains(this)) {
-                    entity.Release(this);
-                }
-            } else {
-                entity.Release(this);
-            }
         }
     }
 }
